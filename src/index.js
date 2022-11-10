b4a57be313358c3debd4aac2fd31d407a8ad3a64
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-10 21:18:42 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
-fetchCountries().then(r => console.log(r));
+fetchCountries().then(r => {
+  const markup = r
+    .map(country => {
+      console.log(country.flags.svg);
+      return `<p class="country-list__text">
+        <img
+          class="country-list__img"
+          src="https://flagcdn.com/ai.svg"
+          width="150px"
+          height="100px"
+          alt=""
+        />
+        Country Name
+      </p>`;
+    })
+    .join('');
+});
