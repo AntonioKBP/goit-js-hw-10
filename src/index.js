f7bb852fa0e21c4b6f7d6fab68a57908c4831e0e
Anton Kovsh <kovsh_anton@ukr.net>
2022-11-11 20:17:32 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,67 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// refs = {};
+
+inputRef.addEventListener('input', onInputText);
+
+function onInputText(evt) {
+  evt.preventDefault();
+  // console.log(evt.currentTarget.value);
+  const inputValue = evt.currentTarget.value.trim();
+  console.log(inputValue);
+
+  fetchCountries()
+    .then(countries => {
+      if (countries.length > 10) {
+        return Notiflix.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      markupCountryList(countries);
+    })
+    .catch(error =>
+      Notiflix.failure('Oops, there is no country with that name')
+    );
+}
+
 fetchCountries().then(r => {
-  const markup = r
+  const markup = r.map(country => {});
+});
+
+function markupCountryList(countries) {
+  const markup = countries
     .map(country => {
-      console.log(country.flags.svg);
       return `<p class="country-list__text">
         <img
           class="country-list__img"
-          src="https://flagcdn.com/ai.svg"
+          src="${country.flags.svg}"
           width="150px"
           height="100px"
           alt=""
         />
-        Country Name
+        ${country.name.official}
       </p>`;
     })
     .join('');
-});
+  countryList.innerHTML = markup;
+}
+
+function markupCountryInfo(countries) {
+  const markup = countries
+    .map(country => {
+      return `<p><img class="country-info__img" src="${country.flags.svg}" alt="" /></p>
+      <p class="country-info__text"><b>Capital: </b>${country.capital}</p>
+      <p class="country-info__text"><b>Population: </b>${country.population}</p>
+      <p class="country-info__text"><b>Languages: </b>${country.languages}</p>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markup;
+}
